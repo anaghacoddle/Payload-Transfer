774bd0fe45dc23c32d0332c7810d5e4fa8df3a36
anaghapcoddle <135127962+anaghapcoddle@users.noreply.github.com>
2024-02-16 18:25:17 +0530

    Delete solid-nextjs-main directory

--- solid-nextjs-main/solid-nextjs-main/_api/fetchPage.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const fetchPage = async (slug: string) => {
-
-  const res = await axios(
-    `${process.env.NEXT_PUBLIC_PAYLOAD_URL}/api/pages?where[slug][equals]=${slug}&depth=2`,
-  )
-  return res
-}
